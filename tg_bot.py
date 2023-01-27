f2344f8bc9bbc2166cf2cdb639da8a9a9e8b13f4
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-01-27 18:58:35 +0200

    Add view basket and rename shop_api

--- tg_bot.py
+++ tg_bot.py
@@ -5,9 +5,9 @@ redis==3.2.1
 """
 from environs import Env
 import redis
-import shop_api
+import elasticpath_shop_api
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (CallbackQueryHandler, CommandHandler, Filters,
                           MessageHandler, Updater)
 
@@ -15,14 +15,16 @@ _database = None
 _shop_token = ''
 
 
-def menu():
-    """
-    Хэндлер для состояния START.
+def start(update, context) :
+    elasticpath_shop_api.get_cart(_shop_token, update.effective_user.id)
+    menu(update.message)
 
-    Бот отвечает пользователю фразой "Привет!" и переводит его в состояние ECHO.
-    Теперь в ответ на его команды будет запускаеться хэндлер echo.
-    """
-    products = shop_api.get_products(_shop_token)
+    return 'HANDLE_MENU'
+
+
+def menu(message):
+    products = elasticpath_shop_api.get_products(_shop_token)
+
     keyboard = [
         [
             InlineKeyboardButton(
@@ -30,74 +32,116 @@ def menu():
         ] for product in products
     ]
     keyboard.append([InlineKeyboardButton("Корзина", callback_data="cart")])
+    markup = InlineKeyboardMarkup(keyboard)
 
-    return InlineKeyboardMarkup(keyboard)
+    message.reply_text(text='Выберите товар:', reply_markup=markup)
 
 
-def start(update, context):
-    markup = menu()
-    shop_api.get_cart(_shop_token, update.effective_user.id)
-    update.message.reply_text(text='Выберите товар:', reply_markup=markup)
+def show_products(message, product_id):
+    product = elasticpath_shop_api.get_product(_shop_token, product_id)
 
-    return 'HANDLE_MENU'
+    keyboard = [
+        [
+            InlineKeyboardButton("1 кг", callback_data=f"{product_id},1"),
+            InlineKeyboardButton("5 кг", callback_data=f"{product_id},5"),
+            InlineKeyboardButton("10 кг", callback_data=f"{product_id},10"),
+        ],
+        [
+            InlineKeyboardButton("Корзина", callback_data="cart")
+        ],
+        [
+            InlineKeyboardButton("Назад", callback_data="back"),
+        ],
+    ]
+    markup = InlineKeyboardMarkup(keyboard)
+    description = "\n".join(
+        [
+            product["name"],
+            "\n",
+            product["description"],
+        ]
+    )
+    message.reply_text(description, reply_markup=markup)
+
+
+def view_cart(message, cart_reference):
+    cart_description = elasticpath_shop_api.get_cart_items(
+        _shop_token, cart_reference)
+    total = cart_description["meta"]["display_price"]["with_tax"]["formatted"]
+    cart_items = []
+
+    for item in cart_description["data"]:
+        cost = item['meta']['display_price']['with_tax']
+        item_description = [
+            item['name'],
+            f"{cost['unit']['formatted']} за кг",
+            f"{item['quantity']} кг на сумму {cost['value']['formatted']}",
+            "\n",
+        ]
+        cart_items.extend(item_description)
+
+    cart_items.append(f"Итого: {total}")
+    cart_text = "\n".join(cart_items)
+
+    keyboard = [[InlineKeyboardButton("Назад", callback_data="back")]]
+    markup = InlineKeyboardMarkup(keyboard)
+
+    message.reply_text(cart_text, reply_markup=markup)
 
 
 def handle_description(update, context):
     query = update.callback_query
     query.answer()
     if query.data == "back":
-        markup = menu()
         query.delete_message()
-        query.message.reply_text(text="Выберите товар:", reply_markup=markup)
+        menu(query.message)
+
         return 'HANDLE_MENU'
+
+    if query.data == "cart":
+        query.delete_message()
+        view_cart(query.message, update.effective_user.id)
+
+        return 'HANDLE_CART'
+
     product_id, quantity = query.data.split(",")
-    shop_api.add_product(_shop_token, update.effective_user.id, product_id, int(quantity)
-                         )
+    elasticpath_shop_api.add_product(
+        token=_shop_token,
+        cart_reference=update.effective_user.id,
+        product_id=product_id,
+        quantity=int(quantity)
+    )
 
     return 'HANDLE_DESCRIPTION'
+
+
+def handle_cart(update, context):
+    query = update.callback_query
+    query.answer()
+    if query.data == "back":
+        query.delete_message()
+        menu(query.message)
+
+        return 'HANDLE_MENU'
+
+    return 'HANDLE_CART'
 
 
 def handle_menu(update, context):
     query = update.callback_query
     query.answer()
-    product_id = query.data
-    product = shop_api.get_product(_shop_token, product_id)
-    keyboard = [
-        [
-            InlineKeyboardButton("1 кг", callback_data=1),
-            InlineKeyboardButton("5 кг", callback_data=5),
-            InlineKeyboardButton("10 кг", callback_data=10),
-        ],
-        [
-            InlineKeyboardButton("Назад", callback_data="back"),
-        ]
-    ]
-    markup = InlineKeyboardMarkup(keyboard)
-    description = "\n".join(
-        [
-            product['name'],
-            "\n",
-            product["description"],
-        ]
-    )
-    query.edit_message_text(text=description, reply_markup=markup)
+    query.delete_message()
+    if query.data == "cart":
+        view_cart(query.message, update.effective_user.id)
+
+        return 'HANDLE_CART'
+
+    show_products(query.message, product_id=query.data)
 
     return 'HANDLE_DESCRIPTION'
 
 
 def handle_users_reply(update, context):
-    """
-    Функция, которая запускается при любом сообщении от пользователя и решает как его обработать.
-    Эта функция запускается в ответ на эти действия пользователя:
-        * Нажатие на inline-кнопку в боте
-        * Отправка сообщения боту
-        * Отправка команды боту
-    Она получает стейт пользователя из базы данных и запускает соответствующую функцию-обработчик (хэндлер).
-    Функция-обработчик возвращает следующее состояние, которое записывается в базу данных.
-    Если пользователь только начал пользоваться ботом, Telegram форсит его написать "/start",
-    поэтому по этой фразе выставляется стартовое состояние.
-    Если пользователь захочет начать общение с ботом заново, он также может воспользоваться этой командой.
-    """
     db = get_database_connection()
     if update.message:
         user_reply = update.message.text
@@ -116,11 +160,10 @@ def handle_users_reply(update, context):
         'START': start,
         'HANDLE_MENU': handle_menu,
         'HANDLE_DESCRIPTION': handle_description,
+        'HANDLE_CART': handle_cart
     }
     state_handler = states_functions[user_state]
-    # Если вы вдруг не заметите, что python-telegram-bot перехватывает ошибки.
-    # Оставляю этот try...except, чтобы код не падал молча.
-    # Этот фрагмент можно переписать.
+
     try:
         next_state = state_handler(update, context)
         db.set(chat_id, next_state)
@@ -129,11 +172,7 @@ def handle_users_reply(update, context):
 
 
 def get_database_connection():
-    """
-    Возвращает конекшн с базой данных Redis, либо создаёт новый, если он ещё не создан.
-    """
-    env = Env()
-    env.read_env()
+
     global _database
     if _database is None:
         database_password = env("REDIS_DB_PASSWORD")
@@ -148,7 +187,7 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     client_id = env("ELASTICPATH_CLIENT_ID")
-    _shop_token = shop_api.get_access_token(client_id)
+    _shop_token = elasticpath_shop_api.get_access_token(client_id)
     telegram_api_key = env("TELEGRAM_API_KEY")
     updater = Updater(telegram_api_key)
     dispatcher = updater.dispatcher
